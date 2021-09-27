fe0d04f52c6fccc695f80bbadb8a49d022d4f162
Your Name <you@example.com>
2021-09-27 15:16:45 -0500

    Lista de respuestas añadida

--- main.py
+++ main.py
@@ -15,3 +15,21 @@ questions = {
     14: '¿Qué cantante es conocido como "The Boss"?',
     15: '¿Qué es lo más caro del mundo?'
 }
+
+answers = {
+    1: ['A', 'a) Cristóbal Colón b) Hernán Cortés\nc) William Sheakspeare d) Rene Descártes', 'a) Cristobal Colón  d) René Descartes'],
+    2: ['D', 'a) 12.23 b) 89.78\nc) 76.87 d) 3.14', 'b) 89.78 d) 3.14'],
+    3: ['A', 'a) Lionel Messi b) Pelé\nc) Cristiano Ronaldo d) La Chofis', 'a) Lionel Messi  c) Cristiano Ronaldo'],
+    4: ['D', 'a) Elefante b) Jirafa\nc) Tiburón blanco d) Ballena azul', 'c) Tiburón blanco  d) Ballena Azul'],
+    5: ['C', 'a) 100 años b) 101 años\nc) 116 años d) 126 años', 'a) 100 años  d) 116 años'],
+    6: ['C', 'a) 1939-1945 b) 1978-1983\nc) 1914-1918 d) 1920-1925', 'a) 1939-1945  c) 1914-1918'],
+    7: ['D', 'a) 100 b) 987\nc) 398 d) 206', 'b) 987  d) 206'],
+    8: ['D', 'a) Aspirina b) Paracetamol\nc) Ramipiril d) Ninguno', 'c) Ramipiril  d) Ninguno'],
+    9: ['B', 'a) Cero b) Uno\nc) El mismo número d) Diez', 'b) Uno  c) El mísmo número'],
+    10: ['B', 'a) 1 billon de años b) 13.8 billones de años\nc) 100 billones de años d) 13 billones de años', 'a) 1 billón de años  d) 13.8 billones de años'],
+    11: ['B', 'a) Islas Marshall b) Ciudad del Vaticano\nc) Palau d) San Marino', 'a) Islas Marshall  b) Ciudad del Vaticano'],
+    12: ['A', 'a) Thriller de Michael Jackson b) Back in Black de AC/DC\nc) El guardaespaldas de Witney Houston d) The Dark Side of the Moon de Pink Floyd', 'a) Thriller de Michael Jackson b) Back in Black de AC/DC'],
+    13: ['D', 'a) Plastico b) Hule\nc) Hojas d) Piel', 'a) Plastico d) Piel'],
+    14: ['D', 'a) Bruno Mars b) Freddie Mercury\nc) Michael Jackson d) Bruce Springsteen', 'a) Bruno Mars d) Bruce Springsteen'],
+    15: ['B', 'a) Polvo lunar b) Antimateria\nc) Oro d) Uranio', 'a) Polvo lunar b) Antimateria']
+}
