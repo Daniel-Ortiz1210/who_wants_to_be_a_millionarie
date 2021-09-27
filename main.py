3a0fc452ee3125de53a148b9c563cf3af1148ca1
Your Name <you@example.com>
2021-09-27 15:13:14 -0500

    Lista de preguntas añadida

---
+++ main.py
@@ -0,0 +1,17 @@
+questions = {
+    1: '¿Quien descubrió América según los libros de historia?',
+    2: '¿Cuánto vale Pi?',
+    3: '¿Quien es el futbolista con más balones de oro?',
+    4: '¿Cuál es el animal más grande en la tierra?',
+    5: '¿Cuánto duró la guerra de los 100 años?',
+    6: '¿Qué periodo de años comprendió la primera guerra mundial?',
+    7: '¿Cuántos huesos tiene el cuerpo humano?',
+    8: '¿Qué medicamento mata a los virus?',
+    9: '¿Cuál es el resultado de cualquier número elevado a 0?',
+    10: '¿Cuál es la edad del universo?',
+    11: '¿Qué país tiene la menor cantidad de población del mundo?',
+    12: '¿Cuál es el album musical más vendido en la historia?', 
+    13: '¿Con qué se fabricaba el pergamino?',
+    14: '¿Qué cantante es conocido como "The Boss"?',
+    15: '¿Qué es lo más caro del mundo?'
+}
